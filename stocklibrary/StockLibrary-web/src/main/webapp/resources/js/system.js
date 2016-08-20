6188b1d0d788594bb3bfc6bee8c9fb08efc27fa3
Higor Senna <higorr_jf@hotmail.com>
2016-08-20 13:38:51 -0300

    criado funcionalidade para adicionar textarea dinamicamente na tela de cadastro de material

---
+++ stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
@@ -0,0 +1,7 @@
+var i = 0;
+$("#botaoAdicionarParagrafo").click(function(){
+	var id = "id = textAreaConteudo"+i;
+	var textArea = "Paragrafo:"+ (i+1) +"<textarea class='form-control' rows='20' style='height: 39%; width: 52%;'" + id + ">";
+	$("#divCadastroMaterial").append("<br />" + textArea);
+	i++;
+});
