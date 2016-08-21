0d75f3495e6ce76d842ff51da28e06e98b31315b
Higor Senna <higorr_jf@hotmail.com>
2016-08-21 14:37:58 -0300

    ajustado executar bean através do js.

--- stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
+++ stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
@@ -1,11 +1,17 @@
-var i = 0;
+window.i = 0;
 $("#botaoAdicionarParagrafo").click(function(){
 	var id = "id = textAreaConteudo"+i;
 	var textArea = "Paragrafo:"+ (i+1) +"<textarea class='form-control' rows='20' style='height: 39%; width: 52%;'" + id + ">";
-	$("#divCadastroMaterial").append("<br />" + textArea);
-	i++;
+	$("#divCadastroMaterial").append("<br />" + textArea);	
+	i++;	
 });
 
-$("#botaoCadastroMaterial").click(function(){
-	'#{cadastroMaterialController.salvar()}';
-});
+function prepararInsercao() {
+	var textoCompleto ="<p> " + $("#textAreaConteudo").val() + " </p>";
+	for(j = 0;j < window.i ;j++){		
+		var idCompleto = "#textAreaConteudo"+j;
+		textoCompleto += "<p> " +  $(idCompleto).val() + " </p>";
+	}
+	alert(textoCompleto);	
+}
+//$("#botaoCadastroMaterialHidden").click();
