0bacc03bada59d18c6fde845d201bc518606da89
hschaves <hschaves@STFJFAC035557-W.stefanini.dom>
2016-08-22 14:27:06 -0300

    criado ação de remover paragrafo e exibir mensagem sucesso

--- stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
+++ stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
@@ -1,14 +1,19 @@
 window.i = 0;
 $("#botaoAdicionarParagrafo").click(function(){
+	i++;
 	var id = "id = textAreaConteudo"+i;
-	var textArea = "Paragrafo:"+ (i+1) +"<textarea class='form-control' rows='20' style='height: 39%; width: 52%;'" + id + ">";
-	$("#divCadastroMaterial").append("<br />" + textArea);	
-	i++;	
+	var tagH5 = "<h5 id=paragrafo" + i + "> Paragrafo:"+ i +"</h5>";
+	var aberturaTagTextArea = "<textarea class='form-control' rows='20' style='height: 39%; width: 52%;'";
+	var fechamentoTagTextArea = "/>";
+	
+	var textAreaFinal = tagH5 + aberturaTagTextArea + id + fechamentoTagTextArea;
+	$("#divCadastroMaterial").append(textAreaFinal);	
+		
 });
 
 function prepararInsercao() {
 	var textoCompleto ="<p> " + $("#textAreaConteudo").val() + " </p>";
-	for(j = 0;j < window.i ;j++){		
+	for(j = 0;j <= window.i;j++){		
 		var idCompleto = "#textAreaConteudo"+j;
 		textoCompleto += "<p> " +  $(idCompleto).val() + " </p>";
 	}
@@ -17,4 +22,15 @@ function prepararInsercao() {
   $("#textoCompleto").val(textoCompleto); 
   
   $("#botaoCadastroMaterialHidden").click();
+}
+
+function removerParagrafo(){
+	if(i > 0){
+		var idTextoCompleto = "#textAreaConteudo"+(i);
+		$(idTextoCompleto).remove();
+		$("#paragrafo"+i).remove();
+		$("#espacoText"+i).remove();
+		window.i = i-1;
+	}
+	
 }
