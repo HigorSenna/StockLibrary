23a87d9e433fdd8ea63113327d4713a745dadfdd
hschaves <hschaves@STFJFAC035557-W.stefanini.dom>
2016-08-23 09:57:45 -0300

    Adicionado validação de required para os campos da tela de cadastro de material

--- stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
+++ stocklibrary/StockLibrary-web/src/main/webapp/resources/js/system.js
@@ -11,18 +11,18 @@ $("#botaoAdicionarParagrafo").click(function(){
 });
 
 function prepararInsercao() {
-	var textoCompleto ="<p>" + $("#textAreaConteudo").val() + "</p>";
+	if(isCampoValido($("#textAreaConteudo").val())){
+		var textoCompleto ="<p>" + $("#textAreaConteudo").val() + "</p>";
+	}
+	
 	for(j = 1;j <= window.i;j++){		
 		var idCompleto = "#textAreaConteudo"+j;
 		textoCompleto += "<p> " +  $(idCompleto).val() + " </p>";
 	}
-	alert(textoCompleto);	
-  if(isCampoValido(textoCompleto)){
-	  $("#textoCompleto").val(textoCompleto); 
+	
+	  $("#Conteudo").val(textoCompleto); 
 	  
 	  $("#botaoCadastroMaterialHidden").click();
-  }
-  
 }
 
 function removerParagrafo(){
@@ -36,21 +36,10 @@ function removerParagrafo(){
 }
 
 function isCampoValido(campo){
-	if(trim(campo) == "<p></p>"){
-		$("#divCadastroMaterial").prepend("" +
-				"<div class='ui-messages-error ui-corner-all'>" +
-					"<a href='#' class='ui-messages-close' onclick='$(this).parent().slideUp();return false;'><span class='ui-icon ui-icon-close'></span></a>" 
-				   +"<span class='ui-messages-error-icon'></span>" 
-				   +"<ul>"
-				   		+"<li>" 
-				   			+"<span class='ui-messages-error-summary'></span>"
-				   			+ "<span class='ui-messages-error-detail'>Campo obrigatório: Paragrafo</span>" 
-				   		+"</li>" 
-				    +"</ul>" 
-				 +"</div>");
-		return false;
+	if(trim(campo) != ""){
+		return true;
 	}
-	return true;
+	return false;
 }
 
 function trim(str) {
